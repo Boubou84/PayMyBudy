7faeef4445ff247e15955123a5809bc06896057f
Pascal <iron82@outlook.fr>
2023-12-01 16:39:26 +0100

    Mise à jour du script SQL pour inclure de nouvelles tables

--- paymybuddy/sql_scripts/create_tables.sql
+++ paymybuddy/sql_scripts/create_tables.sql
@@ -6,8 +6,7 @@ CREATE TABLE Users (
     password VARCHAR(255) NOT NULL,
     full_name VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
-    creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    balance DECIMAL(10, 2) DEFAULT 0.00
+    creation_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Transactions (
@@ -17,7 +16,45 @@ CREATE TABLE Transactions (
     amount DECIMAL(10, 2) NOT NULL,
     transaction_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(50),
-    fee DECIMAL(10, 2),
     FOREIGN KEY (sender_id) REFERENCES Users(user_id),
     FOREIGN KEY (receiver_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE TransactionLogs (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    transaction_id INT,
+    status VARCHAR(50),
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    details TEXT,
+    FOREIGN KEY (transaction_id) REFERENCES Transactions(transaction_id)
+);
+
+CREATE TABLE FailedTransactions (
+    failed_transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    original_transaction_id INT,
+    failed_reason TEXT,
+    failed_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (original_transaction_id) REFERENCES Transactions(transaction_id)
+);
+
+CREATE TABLE CompensationTransactions (
+    compensation_id INT AUTO_INCREMENT PRIMARY KEY,
+    original_transaction_id INT,
+    compensation_amount DECIMAL(10, 2),
+    compensation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (original_transaction_id) REFERENCES Transactions(transaction_id)
+);
+
+CREATE TABLE AccountBalances (
+    balance_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    balance DECIMAL(10, 2),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE TransactionFees (
+    fee_id INT AUTO_INCREMENT PRIMARY KEY,
+    transaction_id INT,
+    fee_amount DECIMAL(10, 2),
+    FOREIGN KEY (transaction_id) REFERENCES Transactions(transaction_id)
 );
